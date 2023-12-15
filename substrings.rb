6a4108ea89c7d5da4e7a1cb2d6cc6f968efa9dc0
JeffreyDinackus <jeffreydinackus@proton.me>
2023-12-14 20:37:19 -0500

    project done

---
+++ substrings.rb
@@ -0,0 +1,34 @@
+
+
+def substrings (string, dictionary)
+
+  result = {}
+  split = string.split
+
+  dictionary.each do |word|
+    split.each do |word2|
+      if word == word2
+        if result[word] != nil
+          result[word] += 1
+
+        else
+
+          result[word] = 1
+        end
+      end
+      
+    end
+  end
+
+
+  result
+end
+
+
+
+dict = "below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"
+
+
+string = "I am going down to the river howdy howdy"
+
+puts substrings(string,dict)
